9ae4cfd3601b834482558e8c384771a1e33d4bee
Yana T <yana.work.related@gmail.com>
2023-11-20 13:36:45 -0800

    added dynamoDB config

--- src/main/java/springapi/SpringApiApplication.java
+++ src/main/java/springapi/SpringApiApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringApiApplication {
+public class
+SpringApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
